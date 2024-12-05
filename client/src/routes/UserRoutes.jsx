3a0b18b6d8bf2cfccec369e58321e8afa0006ede
calebsenm <calebsenm@gmail.com>
2024-12-05 15:12:26 -0500

    Historial de Prestamos OK

--- client/src/routes/UserRoutes.jsx
+++ client/src/routes/UserRoutes.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import UserLayout from "../components/Layout/UserLayout";
 import ProtectedRoute from "./ProtectedRoute";
 import Libro from "../components/User/Libro";
+import Historial from "../components/User/Historial";
 
 const UserRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const UserRoutes = () => {
         <Routes>
            
           <Route path="/libro" element={ <Libro/> } />
+          <Route path="/historial" element={ <Historial/> } />
         </Routes>
       </UserLayout>
     </ProtectedRoute>
